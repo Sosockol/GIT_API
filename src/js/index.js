204bac6274a90c86cd62e45bacd190af0991dac0
Dmitrii Bolokhontsev <bolokhontsev.main@gmail.com>
2024-03-19 15:27:18 +0300

    функция для получения репов по имени

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,26 @@
 import '../scss/style.scss'
 
-console.log('Works!')
+
+
+fetchDirectorys('redux');
+
+function addDirectory() {
+
+}
+
+function fetchDirectorys(querry) {
+  const directorys = fetch('https://api.github.com/search/repositories?q=' + querry + '%20in:name');
+
+  if (directorys.then(res => res.ok)) {
+    directorys.then(res => res.json().then(res => {
+      for (let i = 0; i < 5; i++) {
+        console.log(
+          'name: ' + res.items[i].name +
+          ' owner:' + res.items[i].owner.login +
+          ' stars:' + res.items[i].stargazers_count);
+      }
+    }));
+  } else {
+    alert("Ошибка HTTP: " + directorys.status);
+  }
+}
